a3e7c11e64f08a9a88b1624ed4908c18627920c1
Candice Chin <chincandice2007@gmail.com>
2019-12-20 21:29:48 -0800

    npm build

--- googlebooksearch/src/components/Jumbotron.js
+++ googlebooksearch/src/components/Jumbotron.js
@@ -4,7 +4,7 @@ function Jumbotron() {
     return (
         <div className="container mt-3 text-center">
             <div className="jumbotron jumbotron-fluid bg-info">
-                <h1 className="display-4 font-weight-bold text-white">(REACT) GOOGLE BOOK SEARCH</h1>
+                <h1 className="display-4 font-weight-bold text-white">React-style Google Book Search</h1>
                 <h3 className="font-weight-bold text-white">Search for and Save Books of Interest</h3>
             </div>
         </div>
